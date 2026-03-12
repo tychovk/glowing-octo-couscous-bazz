cffaab00a4726c793b3b571fd0696fb1c6158f9d
Tycho van Kleef <tychovk@gmail.com>
2026-03-12 10:09:46 +0100

    removed lutris

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,9 +12,6 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux
 
-# App to manage game collection
-dnf5 install -y lutris
-
 # Throttlestop: install msr-tools
 dnf5 install -y msr-tools
 
